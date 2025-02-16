50e19c399e18a3bdfad4c77beab666d7f5fc1bcf
BOOBALAN <159717666+Boobalan77@users.noreply.github.com>
2025-02-16 16:01:33 +0530

    Create CollectionAPI_11.java

---
+++ Weekly Task-3/CollectionAPI_11.java
@@ -0,0 +1,15 @@
+
+
+import java.util.LinkedList;
+
+public class AppendToLinkedList {
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<>();
+		list.add("Red");
+		list.add("Blue");
+		list.add("Green");
+
+		list.addLast("Yellow"); // Append to the end
+		System.out.println("Updated LinkedList: " + list);
+	}
+}
