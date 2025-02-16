415384e092bd19d9c2aa267398231550c0048511
BOOBALAN <159717666+Boobalan77@users.noreply.github.com>
2025-02-16 15:53:38 +0530

    Create CollectionAPI_1.java

---
+++ Weekly Task-3/CollectionAPI_1.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorList {
+	public static void main(String[] args) {
+		List<String> colors = new ArrayList<>();
+		colors.add("Red");
+		colors.add("Blue");
+		colors.add("Green");
+		colors.add("Yellow");
+
+		System.out.println("Color List: " + colors);
+	}
+}
